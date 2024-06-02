3d3a8292c669df3d7afd120a373d188a7a9be11b
Jpdl925 <jtpg925@gmail.com>
2024-06-02 11:58:28 -0700

    first commit

---
+++ registerApp/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from "./components/Login"
+import Register from "./components/Register"
+
+const App = () => {
+  return (
+    <>
+    <Register/>
+    <Login/>
+    </>
+  )
+}
+
+export default App
