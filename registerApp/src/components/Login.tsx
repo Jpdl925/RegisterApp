3bcfcc63401d39d951fab01ed24939a37b41eee3
Jpdl925 <jtpg925@gmail.com>
2024-06-02 13:56:22 -0700

    Register validation complete

--- registerApp/src/components/Login.tsx
+++ registerApp/src/components/Login.tsx
@@ -1,8 +1,34 @@
 
+import { FieldValues, useForm } from "react-hook-form"
+
 const Login = () => {
-  return (
+
+    
+    const {register,getValues,handleSubmit, formState:{errors}} = useForm();
+    console.log(errors);
+
+const onHelpSubmit = (data:FieldValues) => {
+    console.log(data)
+    
+}
+
+return (
     <>
-    <h1>test2</h1>
+    <h1>Login Form</h1>
+    <form onSubmit={handleSubmit(onHelpSubmit)}>
+
+        <label>Email:</label>
+        <input {...register('email', {required:true,minLength:3})}  id="email" type="text"></input>
+        {errors.email?.type === 'required' && <p className="text-danger">Email field is required</p>}
+        {errors.email?.type === 'minLength' && <p className="text-danger">Email must be at least 3 characters long</p>}
+
+        <label>Password:</label>
+        <input {...register('pWord', {required:true,minLength:3})}   id="pWord" type="text"></input>
+        {errors.pWord?.type === 'required' && <p className="text-danger">Password field is required</p>}
+        {errors.pWord?.type === 'minLength' && <p className="text-danger">Password must be at least 3 characters long</p>}
+
+        <button  type="submit">Submit</button>
+    </form>
     </>
   )
 }
--- registerApp/src/components/Register.tsx
+++ registerApp/src/components/Register.tsx
@@ -1,8 +1,55 @@
 
+import { FieldValues, useForm } from "react-hook-form"
+
+
+
 const Register = () => {
+
+
+    
+
+    const {register,getValues,handleSubmit, formState:{errors}} = useForm();
+    console.log(errors);
+
+const onHelpSubmit = (data:FieldValues) => {
+    console.log(data)
+    
+}
+
+    
+
   return (
     <>
-    <h1>test</h1>
+    <h1>Register Form</h1>
+    <form onSubmit={handleSubmit(onHelpSubmit)}>
+
+        <label>First Name:</label>
+        <input {...register('fName', {required:true,minLength:3})} id="fName" type="text"></input>
+        {errors.fName?.type === 'required' && <p className="text-danger">Fist Name field is required</p>}
+        {errors.fName?.type === 'minLength' && <p className="text-danger">First Name must be at least 3 characters long</p>}
+
+        <label>Last Name:</label>
+        <input {...register('lName', {required:true,minLength:3})} id="lName" type="text"></input>
+        {errors.lName?.type === 'required' && <p className="text-danger">Last Name field is required</p>}
+        {errors.lName?.type === 'minLength' && <p className="text-danger">Last Name must be at least 3 characters long</p>}
+
+        <label>Email:</label>
+        <input {...register('email', {required:true,minLength:3})}  id="email" type="text"></input>
+        {errors.email?.type === 'required' && <p className="text-danger">Email field is required</p>}
+        {errors.email?.type === 'minLength' && <p className="text-danger">Email must be at least 3 characters long</p>}
+
+        <label>Password:</label>
+        <input {...register('pWord', {required:true,minLength:3})}   id="pWord" type="text"></input>
+        {errors.pWord?.type === 'required' && <p className="text-danger">Password field is required</p>}
+        {errors.pWord?.type === 'minLength' && <p className="text-danger">Password must be at least 3 characters long</p>}
+
+        <label >Confirm Password:</label>
+        <input {...register('cWord', {required:true})}  id="cWord" type="text"></input>
+        {errors.cWord?.type === 'required' && <p className="text-danger">Please Confirm Password</p>}
+        {getValues('pWord') !== getValues('cWord') && <p>Passwords do not match</p>}
+
+        <button  type="submit">Submit</button>
+    </form>
     </>
   )
 }
